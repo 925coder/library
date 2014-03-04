1482897235946570db818688e835c46ca6ea4a63
Ilias Shaikh <ilias.shaikh@tradar.com>
2014-03-04 14:38:51 +0000

    fixed build.

---
+++ NetEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coder925.library
+{
+  public static class NetEx
+  {
+    public static string GetCurrentHostName()
+    {
+      return Environment.MachineName;
+    }
+  }
+}
